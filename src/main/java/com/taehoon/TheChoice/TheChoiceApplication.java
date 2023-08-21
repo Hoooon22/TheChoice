9c05a9f08b8103e8277ad2b8f82ca95ae477ff7d
momo990305@gmail.com <momo990305@gmail.com>
2023-08-21 23:33:15 +0900

    First Commit

---
+++ src/main/java/com/taehoon/TheChoice/TheChoiceApplication.java
@@ -0,0 +1,13 @@
+package com.taehoon.TheChoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheChoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheChoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taehoon/TheChoice/TheChoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taehoon.TheChoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheChoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
