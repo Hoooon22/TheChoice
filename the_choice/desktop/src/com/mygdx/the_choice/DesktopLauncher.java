aea4265ca7547cc59a4ff72cc74ac8ab2a30f415
TaehyeonJeon <taehyun0540@gmail.com>
2023-08-22 16:55:13 +0900

    edit title

--- the_choice/desktop/src/com/mygdx/the_choice/DesktopLauncher.java
+++ the_choice/desktop/src/com/mygdx/the_choice/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("the_choice");
+		config.setTitle("The_choice");
 		new Lwjgl3Application(new TheChoice(), config);
 	}
 }
